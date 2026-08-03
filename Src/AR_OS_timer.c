256a46887ae29422a89ef3041406b2021985bfc5
Luismel <luismelpinaless@gmail.com>
2026-08-03 12:09:56 -0400

    Refactor timer functions to include mode parameter
    Null pointer verification implementation

--- Src/AR_OS_timer.c
+++ Src/AR_OS_timer.c
@@ -1,37 +1,55 @@
-#include <AR_timer.h>
+#include <AR_OS_timer.h>
 
-void timer_init(TIMER *tm, uint32_t limit){
+void timer_init(TIMER *tm, uint32_t limit, bool mod){
+if(tm == NULL){
+	return;
+}
+
 tm->counter = 0;
 tm->limit = limit;
 tm->active = true;
+tm->mode = mod;
 tm->period_reached = false;
 tm->event = EVT_TIMER_IDLE;
 }
 
 void timer_set(TIMER *tm){
+if(tm == NULL){
+	return;
+}
+
 if(tm->active == false){
 	tm->active = true;
 	}
 }
 
 void timer_reset(TIMER *tm){
+if(tm == NULL){
+	return;
+}
+
 if(tm->active == true){
 	tm->active = false;
 	}
 }
 
 void timer_update(TIMER *tm){
+if(tm == NULL){
+	return;
+}
+
 if(tm->active == false){
 tm->counter = 0;
 tm->event = EVT_TIMER_IDLE;
+tm->period_reached = false;
 return;
 }
 
-if(tm->event == EVT_TIMER_DONE){
+if(tm->event == EVT_TIMER_DONE && tm->mode == true){
 	tm->event = EVT_TIMER_IDLE;
 }
 
-if(tm->period_reached == true){
+if(tm->period_reached == true && tm->mode == true){
 	tm->period_reached = false;
 }
 
